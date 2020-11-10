cad029e9e3bd7eb5c5509396855caac84a7cb2ef
Jeff Cleverley <jeff@jeffcleverley.com>
2020-11-10 21:22:33 +0800

    disable rest endpoints for user enumeration

--- gridpane-block-username-enumeration/gridpane-block-username-enumeration.php
+++ gridpane-block-username-enumeration/gridpane-block-username-enumeration.php
@@ -4,3 +4,13 @@ if (!is_admin() && isset($_REQUEST['author'])) {
 	die();
 }
 
+function disable_rest_endpoints ( $endpoints ) {
+	if ( isset( $endpoints['/wp/v2/users'] ) ) {
+		unset( $endpoints['/wp/v2/users'] );
+	}
+	if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+		unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+	}
+	return $endpoints;
+}
+add_filter( 'rest_endpoints', 'disable_rest_endpoints');
