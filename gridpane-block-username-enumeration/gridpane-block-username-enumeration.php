1758f6132d33fcac5148ef9eb387aea35f77ab09
Jeff Cleverley <jeff@jeffcleverley.com>
2021-07-21 17:02:11 +0800

    jeff starr

--- gridpane-block-username-enumeration/gridpane-block-username-enumeration.php
+++ gridpane-block-username-enumeration/gridpane-block-username-enumeration.php
@@ -1,7 +1,13 @@
 <?php
-if (!is_admin() && isset($_REQUEST['author'])) {
-	status_header(404);
-	die();
+if (!is_admin()) {
+	// default URL format
+	if (preg_match('/author=([0-9]*)/i', $_SERVER['QUERY_STRING'])) die();
+	add_filter('redirect_canonical', 'shapeSpace_check_enum', 10, 2);
+}
+function shapeSpace_check_enum($redirect, $request) {
+	// permalink URL format
+	if (preg_match('/\?author=([0-9]*)(\/*)/i', $request)) die();
+	else return $redirect;
 }
 
 function disable_rest_endpoints ( $endpoints ) {
