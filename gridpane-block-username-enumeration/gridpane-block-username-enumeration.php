5a88429c97ccc62efde3632f3d7c6bde40d0e76b
Jeff Cleverley <jeff@jeffcleverley.com>
2021-12-13 16:27:28 +0800

    release

--- gridpane-block-username-enumeration/gridpane-block-username-enumeration.php
+++ gridpane-block-username-enumeration/gridpane-block-username-enumeration.php
@@ -1,5 +1,5 @@
 <?php
-if (!is_admin()) {
+if (! is_admin()) {
 	// default URL format
 	if (preg_match('/author=([0-9]*)/i', $_SERVER['QUERY_STRING'])) die();
 	add_filter('redirect_canonical', 'shapeSpace_check_enum', 10, 2);
@@ -8,6 +8,11 @@ function shapeSpace_check_enum($redirect, $request) {
 	// permalink URL format
 	if (preg_match('/\?author=([0-9]*)(\/*)/i', $request)) die();
 	else return $redirect;
+}
+
+if (! strpos($_SERVER['HTTP_REFERER'],  '://' . $_SERVER['HTTP_HOST'] . '/wp-admin')) {
+	// remove rest endpoints IF the referer string does not contain ://host.site/wp-admin
+	add_filter('rest_endpoints', 'disable_rest_endpoints');
 }
 
 function disable_rest_endpoints ( $endpoints ) {
@@ -19,4 +24,4 @@ function disable_rest_endpoints ( $endpoints ) {
 	}
 	return $endpoints;
 }
-add_filter( 'rest_endpoints', 'disable_rest_endpoints');
+
