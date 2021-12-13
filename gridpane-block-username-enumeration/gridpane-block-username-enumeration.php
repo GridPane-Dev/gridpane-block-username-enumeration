dc7932238b33a69a176886a7de0f7244928fde60
Jeff Cleverley <jeff@jeffcleverley.com>
2021-12-13 21:56:17 +0800

    release

--- gridpane-block-username-enumeration/gridpane-block-username-enumeration.php
+++ gridpane-block-username-enumeration/gridpane-block-username-enumeration.php
@@ -1,25 +1,36 @@
 <?php
-if (! is_admin()) {
+if (! is_admin())
+{
 	// default URL format
 	if (preg_match('/author=([0-9]*)/i', $_SERVER['QUERY_STRING'])) die();
 	add_filter('redirect_canonical', 'shapeSpace_check_enum', 10, 2);
 }
-function shapeSpace_check_enum($redirect, $request) {
+function shapeSpace_check_enum($redirect, $request)
+{
 	// permalink URL format
 	if (preg_match('/\?author=([0-9]*)(\/*)/i', $request)) die();
 	else return $redirect;
 }
 
-if (! strpos($_SERVER['HTTP_REFERER'],  '://' . $_SERVER['HTTP_HOST'] . '/wp-admin')) {
-	// remove rest endpoints IF the referer string does not contain ://{host.site}/wp-admin
-	add_filter('rest_endpoints', 'disable_rest_endpoints');
+function disable_users_rest_if_not_logged_in()
+{
+	// If request is not from logged in user, then disable users rest endpoint
+	if (! is_user_logged_in() )
+	{
+		add_filter('rest_endpoints', 'disable_users_rest_endpoints');
+	}
 }
+add_action('init', 'disable_users_rest_if_not_logged_in');
 
-function disable_rest_endpoints ( $endpoints ) {
-	if ( isset( $endpoints['/wp/v2/users'] ) ) {
+function disable_users_rest_endpoints ( $endpoints )
+{
+	if ( isset( $endpoints['/wp/v2/users'] ) )
+	{
 		unset( $endpoints['/wp/v2/users'] );
 	}
-	if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+
+	if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) )
+	{
 		unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
 	}
 	return $endpoints;
